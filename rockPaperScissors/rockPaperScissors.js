91f1ad54f83ef548d75e683dcb00f2d59154173a
Joe Prempeh <prempehjoe@gmail.com>
2015-04-28 09:45:04 -0700

    Solution found

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,30 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (rounds) {
+    // TODO: your solution here
+    // Given that we have a number of rounds
+    // and there are three possible solutions per round (rock paper scissors)
+    // we should randomly decide what the throw is
+    // and push it into an array then after doing that for the number of rounds
+    // return a result
+    var results         = [],
+        possibleResults = ["rock", "papers", "scissors"],
+        decideResult    = possibleResults[Math.floor(Math.random() * possibleResults.length)],
+        decideResult2    = function(){
+            var newOne = possibleResults[Math.floor(Math.random() * possibleResults.length)];
+            return newOne;
+        },
+        round = rounds,
+        play = function(round)
+        {
+            var counter         = 0;
+            while (counter < round){
+                counter++;
+                results.push(decideResult2());
+            }
+            console.log(results);
+            return results;
+        };
+    play(rounds);
 };
