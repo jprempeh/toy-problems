c4b9fad8127ec3d8a51ace4d3b7a1673aec7689e
Joe Prempeh <prempehjoe@gmail.com>
2015-04-29 10:02:16 -0700

    as far as i got

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -11,5 +11,31 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+    // we're getting an array of numbers
+    // we should keep track of how often each number is in the array (maybe use an object)
+    var occurances = {};
+    var evens = [];
+    for (var i = 0; i < arr.length; i++) {
+        occurances[arr[i]] = (occurances[arr[i]] || 0) + 1;
+    }
+
+    for (var prop in occurances) {
+        if (occurances[prop] % 2 === 0) {
+            evens.push(prop);
+        }
+    }
+    var results = [];
+
+    for (var j = 0; j < evens.length; j++) {
+        results.push(arr.indexOf(evens[j]));
+    }
+
+    return results;
+
+    // as soon as the number occurs 2 times
+    // return that number
+    // if no numbers occur 2 times
+    // return null
 };
+
+
