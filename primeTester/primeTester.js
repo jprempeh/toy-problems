bb4eb5b295aa569511c6c2455c758f2c6f112c53
Joe Prempeh <prempehjoe@gmail.com>
2015-05-02 09:20:02 -0700

    changes

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -19,6 +19,38 @@ var primeTester = function(n) {
  */
 
 var primeSieve = function (start, end) {
+  var primes = [];
+  for (var i = start; i < end; i++) {
+    if (primeTester(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+};
+
+// if start was 10 and end was 15
+// choose two as a prime then
+// go through the whole range two at a time and mark all multiples of two as not prime
+
+
+var primeSieve = function(start, end) {
+  var current = 2;
+  var primes = range(0, end);
+  while (current < end) {
+    // mark all multiples not prime
+    for(var i = current * 2; i <= end; i+= current;) {
+      primes[i] = null;
+    }
+    //find the next currrent
+    current += 1;
+    while(!primes[current] && current <= end) { current ++; }
+  }
+  return primes.slice(2).filter(function (val) { return val && val >= start; });
 };
 
 
+var range = function(start, end) {
+  var result = [];
+  for (var i = start; i < end; i++) { result.push(i); }
+  return result
+};
