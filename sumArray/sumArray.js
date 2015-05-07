ff12f8976c34f9b6c39d6f4afd9eb8fe64983fc6
Joe Prempeh <prempehjoe@gmail.com>
2015-05-07 09:41:34 -0700

    sumArray answered

--- sumArray/sumArray.js
+++ sumArray/sumArray.js
@@ -11,4 +11,22 @@
 
 // Solved in O(n) time with O(1) memory
 var sumArray = function(array) {
+    // create variables to hold the current sum and the maximum possible sum
+    var maximumSum = 0;
+    var currentSum = 0;
+    var arrayLength = array.length;
+
+    // if the array is empty return nothing
+    if (!arrayLength) {
+        return null;
+    }
+    // loop through the array
+    for (var i = 0; i < arrayLength; i++) {
+        // check the current sum by checking the current number and adding it to the current sum and taking the max
+        currentSum = Math.max((currentSum + array[i]), array[i]);
+        // check if the maximum greatest summer would be this current sum or the previous maximum sum
+        maximumSum = Math.max(currentSum, maximumSum);
+    }
+    // return the answer
+    return maximumSum;
 };
