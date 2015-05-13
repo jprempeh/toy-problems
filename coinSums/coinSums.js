f261f9d573186e565fb8dac23f125d924fe5a111
Joe Prempeh <prempehjoe@gmail.com>
2015-05-13 09:57:37 -0700

    Coin sums attempted and pseudocoded

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -26,6 +26,49 @@ makeChange(2) === 2
 
 var makeChange = function(total){
 
+    var currency = [1, 2, 5, 10, 20, 50, 100, 200];
+
+    var currentTotal,
+        results;
+
+    for (var i = 0; i < currency.length; i++) {
+        // total was 2
+        if (currency[i] > total) {
+            break;
+        } else if (currency[i] === total) {
+            results.push(currency[i]);
+        } else {
+            // if the currency piece is less than the total, determine the maximum amount of that piece that you can use
+            // to make it equal or close to the equal and push that value
+            // by default, by starting with 1 pence, you can make one pence * by the total to equal the total
+            if (currency[i] === 1) {
+                results.push(total / currency[i]);
+            } else if (currency[i] === 2) {
+                results.push(total / currency[i]);
+            }
+        }
+    }
+
+    // it seems like you could take the maximum coin that you could use for the total, select one of those coins
+    // and then recurse downward into the next denomination
+    // you will need a recursive function
+    // also it seems like upon taking the maximum coin, you could automatically calculate all the possible ways to
+    // make that maximum coin using the coins below it
+    // for example with (6) you have (1 5p + 1p) and 5p can be broken down into (2 2p +1 1p) or (5 1p)
+
+    return results.length;
+
+
 };
 
 
+def countChange(money: Int, coins: List[Int]): Int = {
+    def reduce(money: Int, coins: List[Int], accCounter: Int): Int = {
+    if(money == 0) accCounter + 1
+    else if(money < 0 || coins.isEmpty) accCounter
+    else reduce(money - coins.head, coins, accCounter) + reduce(money, coins.tail, accCounter)
+}
+
+if(money <= 0 || coins.isEmpty) 0
+else reduce(money, coins, 0)
+}
