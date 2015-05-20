6651fc5ae3d1d9092deeca629401df0a8dbfdaaf
Joe Prempeh <prempehjoe@gmail.com>
2015-05-20 09:58:41 -0700

    better solution, no start argument

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,20 +12,23 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(start, string) {
-    var result = [];
-
-    if (string.length === 1) {
-        result.push(start + string);
-        return result;
-    } else {
-        for(var i = 0; i < string.length; i++){
-            var recurse = allAnagrams(string[i], string.substr(0, i) + string.substr(i + 1));
-            for (var j = 0; j < recurse.length; j++) {
-                result.push(start + recurse[j]);
+var allAnagrams = function(string) {
+    var start = '';
+    var string = string;
+    var inRecurse = function(start, string) {
+        var result = [];
+        if (string.length === 1) {
+            result.push(start + string);
+            return result;
+        } else {
+            for (var i = 0; i < string.length; i++) {
+                var recurse = inRecurse(string[i], string.substr(0, i) + string.substr(i + 1));
+                for (var j = 0; j < recurse.length; j++) {
+                    result.push(start + recurse[j]);
+                }
             }
         }
-    }
-
-    return result;
+        return result;
+    };
+    return inRecurse(start, string);
 };
