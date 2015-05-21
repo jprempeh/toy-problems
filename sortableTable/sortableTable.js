34bc7a1fb598a8551e754b94c6e98a157f398c27
Joe Prempeh <prempehjoe@gmail.com>
2015-05-21 10:03:35 -0700

    Sortable table attempted

--- sortableTable/sortableTable.js
+++ sortableTable/sortableTable.js
@@ -9,7 +9,29 @@
  **/
 
 $(function () {
-  // TODO: your code here!
+    $('th').on('click', function() {
+        var col = this;
+        var rows = $('tbody tr');
+        var rlen = rows.length;
+        var arr = [];
+        var i, j, cells, clen;
 
+        for (i = 0; i < rlen; i++) {
+            cells = rows[i].cells;
+            clen = cells.length;
+            arr[i] = [];
+            for (j = 0; j < clen; j++) {
+                arr[i][j] = cells[j].innerHTML;
+            }
+        }
+        // sort the array by column number
+        arr.sort(function(a,b){
+            return (a[col] == b[col]) ? 0 : ((a[col] > b [col]) ? asc : -1*asc);
+        });
+        for(i = 0; i < rlen; i++) {
+            arr[i] = "<td>" + arr[i].join("</td><td>")+"</td>";
+        }
+        $('tbody').innerHTML = "<tr>" + arr.join("</tr><tr>") + "</tr>";
+    });
 });
 
