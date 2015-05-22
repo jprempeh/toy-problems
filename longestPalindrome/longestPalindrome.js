f3f9f048ce5ff74526b8afa697fd287a445dfa4b
Joe Prempeh <prempehjoe@gmail.com>
2015-05-22 09:47:18 -0700

    (feat): longest Palindrome completed

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -7,4 +7,33 @@
 */
 
 var longestPalindrome = function (string) {
+    var isPalindrome = function (str) {
+        var reverseString = str.split("").reverse().join("");
+        return str == reverseString;
+    };
+
+    var maxPalLength = 0;
+    var maxPal = '';
+
+    for (var i = 0; i < string.length; i++) {
+
+        var subs = string.substr(i, string.length);
+
+        for (var j = subs.length; j >= 0; j--) {
+            var subSubs = subs.substr(0, j);
+            if (subSubs.length <= 1) continue;
+
+            if (isPalindrome(subSubs)) {
+                if (subSubs.length > maxPalLength) {
+                    maxPalLength = subSubs.length;
+                    maxPal = subSubs;
+                }
+            }
+
+        }
+    }
+    return maxPal;
 };
+
+
+console.log(longestPalindrome("My dad"));
