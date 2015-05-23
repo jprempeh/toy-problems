a18e015b0d15ccfdfb459b8de625bb0d0dd2e7d3
Joe Prempeh <prempehjoe@gmail.com>
2015-05-23 09:55:55 -0700

    (feat): multiply and divide implemented

--- implementingOperators/implementingOperators.js
+++ implementingOperators/implementingOperators.js
@@ -11,15 +11,37 @@
 
 
 var multiply = function(x, y) {
-  // TODO: should return the product of x * y
+	// if x = 3 and y = 4
+	// then that's 3 + 3 + 3 + 3
+	var result = 0;
+	for (var i = y; i > 0; i--) {
+		result += x;
+	}
+	return result;
 };
 
 var divide = function(x, y) {
-  // TODO: should return the dividend of x / y
+	// if x is 24 and y is 3 then result should be 8
+	var zero = x;
+	var remainder;
+	var counter = 0;
+	while (zero > 0 && zero >= y) {
+		remainder = x - y;
+		zero -= y;
+		counter++;
+	}
+	return counter;
 };
 
 var modulo = function(x, y) {
-  // TODO: should return the remainder of x / y
+
 };
+
+
+console.log(multiply(3, 10));
+console.log(multiply(10, 10));
+console.log(divide(100, 10));
+console.log(divide(64, 8));
+console.log(divide(82, 9));
 
 
