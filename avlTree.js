4370fc6db05aea1c52cd0c7fe8b198631f5d20eb
Joe Prempeh <prempehjoe@gmail.com>
2015-07-13 09:27:01 -0700

    AVL tree

---
+++ avlTree.js
@@ -0,0 +1,6 @@
+//Implement an AVL tree (aka self-balancing binary tree). In an AVL tree, the heights of the two child subtrees of
+// any node differ by at most one
+//; if at any time they differ by more than one, rebalancing is done to restore this property.
+// Lookup, insertion, and deletion
+// all take O(log n) time in both the average and worst cases, where n is the number of nodes in the
+// tree prior to the operation.
