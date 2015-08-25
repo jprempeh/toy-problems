e3acb507ceed9bf866c0645b24fe40e455ca1309
Joe Prempeh <prempehjoe@gmail.com>
2015-08-25 11:03:45 -0700

    answer

--- isSubsetOf.js
+++ isSubsetOf.js
@@ -1,38 +1,41 @@
-var isSubsetOf = function(input, superset) {
-	var results = [];
-	for(var i = 0; i < input.length; i++) {
-		// loop through input
-		// if the input element is the same as any of the superset
-		// mark in an object that is true
-		for (var j = 0; j < superset.length; j++) {
-			if (input[i] === superset[j]) {
-				results[i] = true;
-				break;
-			} else {
-				results[i] = false;
+
+	Array.prototype.isSubsetOf = function(superset) {
+		var results = {};
+		for(var i = 0; i < this.length; i++) {
+			// loop through input
+			// if the input element is the same as any of the superset
+			// mark in an object that is true
+			for (var j = 0; j < superset.length; j++) {
+				if (this[i] === superset[j]) {
+					results[i] = true;
+					break;
+				} else {
+					results[i] = false;
+				}
 			}
 		}
-	}
-	var container = [];
-	for (var val in results) {
-		if (results[val] === true) {
-			container.push(true);
-		} else {
-			container.push(false);
+		var container = [];
+		for (var val in results) {
+			if (results[val] === false) {
+				return false;
+			} else {
+				container.push(true);
+			}
 		}
-	}
-	var marker = false;
-	for (var k = 0; k < container.length; k++) {
-		if (container[k] === true) {
-			marker = true;
-		} else {
-			marker = false;
+		var marker = false;
+		for (var k = 0; k < container.length; k++) {
+			if (container[k] === true) {
+				marker = true;
+			} else {
+				marker = false;
+			}
 		}
-	}
-	return marker;
-};
+		return marker;
+	};
 
-var result = ['cat', 'dog', 'cow', 'cow', 'poop'];
-var supersuper = ['dog', 'cow', 'fox', 'cat'];
 
-console.log(isSubsetOf(result, supersuper));
+
+var result = ['cat', 'dog', 'cow'];
+var supersuper = ['dog', 'cow', 'fox'];
+
+console.log(result.isSubsetOf(supersuper));
